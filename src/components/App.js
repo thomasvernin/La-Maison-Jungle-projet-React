facdfebf3064ccdb608e8e0ac55be150c558e4c0
Thomas Vernin <tvernin49100@gmail.com>
2024-05-24 18:34:01 +0200

    Derniers push

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,29 @@
+import { useState, useEffect } from 'react'
 import Banner from './Banner'
-// import Cart from './Cart'
-import ShoppingList from './ShoppingList';
-
+import logo from '../assets/logo.png'
+import Cart from './Cart'
+import Footer from './Footer'
+import ShoppingList from './ShoppingList'
+import '../styles/Layout.css'
 
 function App() {
+	const savedCart = localStorage.getItem('cart')
+	const [cart, updateCart] = useState(savedCart ? JSON.parse(savedCart) : [])
+	useEffect(() => {
+		localStorage.setItem('cart', JSON.stringify(cart))
+	}, [cart])
+
 	return (
 		<div>
-			<Banner />
-			{/* <Cart /> */}
-			<ShoppingList />
+			<Banner>
+				<img src={logo} alt='logo-la-maison-jungle' className='lmj-logo' />
+				<h1 className='lmj-title'>La maison jungle</h1>
+			</Banner>
+			<div className='lmj-layout-inner'>
+				<Cart cart={cart} updateCart={updateCart} />
+				<ShoppingList cart={cart} updateCart={updateCart} />
+			</div>
+			<Footer />
 		</div>
 	)
 }
--- src/components/PlantItem.js
+++ src/components/PlantItem.js
@@ -1,9 +1,14 @@
 import CareScale from './CareScale'
-import '../styles/PlantItem.css' ;
+import '../styles/PlantItem.css'
 
-function PlantItem({ id, cover, name, water, light }) {
+function handleClick(plantName) {
+	alert(`Vous voulez acheter 1 ${plantName}? Très bon choix 🌱✨`)
+}
+
+function PlantItem({ cover, name, water, light, price }) {
 	return (
-		<li key={id} className='lmj-plant-item'>
+		<li className='lmj-plant-item' onClick={() => handleClick(name)}>
+			<span className='lmj-plant-item-price'>{price}€</span>
 			<img className='lmj-plant-item-cover' src={cover} alt={`${name} cover`} />
 			{name}
 			<div>
